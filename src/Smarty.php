299c039a5b79563ded54e4b643670fae6fa03d19
邹景立 <zoujingli@qq.com>
2016-10-20 12:03:57 +0800

    添加模板常量

--- src/Smarty.php
+++ src/Smarty.php
@@ -77,9 +77,6 @@ class Smarty {
         $default['__LIB__'] = $default['__ROOT__'] . '/static/plugs';
         $default['__STATIC__'] = $default['__ROOT__'] . '/static';
         $default['__UPLOAD__'] = $default['__ROOT__'] . '/static/upload';
-        echo '<pre>';
-        echo var_export($default, TRUE);
-        exit;
         $this->template->assign($default);
         // 赋值模板变量
         !empty($template) && $this->template->assign($data);
